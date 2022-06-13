4a9ca3838808c8c3415e2370096f45b2242d57f7
PeterMulhair <bspm@leeds.ac.uk>
2022-06-13 14:54:43 +0100

    Update parse script

--- parse_blast.py
+++ parse_blast.py
@@ -23,80 +23,84 @@ parse.add_argument("--path",type=str, help="path to genomes in fasta format",req
 
 args = parse.parse_args()
 
-    
-sp_out = args.taxa.split('/')[-1].split('_')[2].split('.')[0]
-print('Parsing BLAST output\n')
-print(sp_out, '\n')
-#Create dictionary of species genome file
-assemb = args.taxa.split('/')[-1].split('.blast')[0]
-assembly = glob.glob(args.path + assemb + '.f*')
+output_list = []
+for output in glob.glob(args.taxa + '*fa'):
+    output_list.append(output)
 
-contig_seq = {}
-with open(assembly[0]) as f:
-    for record in SeqIO.parse(f, 'fasta'):
-        contigID = record.id
-        seq = str(record.seq)
-        contig_seq[contigID] = seq 
-
+num_sp = len(output_list)
+print('Parsing BLAST output for', num_sp, 'species')
 
 #For each CS gene used in blast, pull out the BLAST coordinates from the output files
 gene_list = ['CSP', 'GOBP', 'GR', 'IR', 'SNMP', 'OBP', 'OR']
 
 #for CS_gene in gene_list:
 def pull_CSseq(CS_gene, rounds):
-    if rounds == 'NULL':
-        new_file = 'results/blastout/sp_blast_extract/' + sp_out + '_' + CS_gene + '.fasta'
-    else:
-        new_file = 'results/blastout/round_' + rounds + '/sp_blast_extract/' + sp_out + '_' + CS_gene + '.fasta'
-        
-    #with open('results/blastout/sp_blast_extract/' + sp_out + '_' + CS_gene + '.fasta', 'w') as outF:
-    with open(new_file, 'w') as outF:
-        print(CS_gene)
-        with open(args.taxa) as f:
-            gene_contigs = defaultdict(list)
-            contig_seq_hit = defaultdict(list)
-            
-            for line in f:
-                lines = line.split('\t')
-                #geneID = lines[0].split('|')[1]
-                gene = lines[0]#.split('0')[0]
-                if CS_gene in gene:
-                    
-                    sp_contig = lines[1]
-                    qstart = lines[5]
-                    qend = lines[6]
-                    qhit = int(qend) - int(qstart)
-                    qlen = lines[7]
-                    q_cov = qhit/int(qlen)
-                    q_cov = 100*q_cov
+    for taxa in output_list:
+        taxa_file = taxa.split('/')[-1]
+        sp_out = taxa.split('/')[-1].split('_')[2].split('.')[0]
 
-                    s_start = lines[8]
-                    s_end = lines[9]
-                    s_len = lines[10]
-                     
-                    contig_seq_hit[sp_contig].append(int(s_start))
-                    contig_seq_hit[sp_contig].append(int(s_end))
-                    gene_contigs[gene].append(sp_contig)
-                    
+        assemb = taxa.split('/')[-1].split('.blast')[0]
+        assembly = glob.glob(args.path + assemb + '.f*')
+        contig_seq = {}
+        with open(assembly[0]) as f:
+            for record in SeqIO.parse(f, 'fasta'):
+                contigID = record.id    
+                seq = str(record.seq)
+                contig_seq[contigID] = seq
 
-            for contig_hit, nuc_hit in contig_seq_hit.items():
-                nuc_start = min(nuc_hit) - 100
-                nuc_end = max(nuc_hit) + 100 #Regions within a contig that had multiple CS gene hits are taken out in a single large sequence for annotation
-                    
-                #print(contig_hit, nuc_start, nuc_end)
-
-                genome_contig = contig_seq[contig_hit]
                 
-                blast_hit = []
-                for i in range(nuc_start, nuc_end):
-                    try:
-                        nuc = genome_contig[i]
-                        blast_hit.append(nuc)
-                    except:
-                        continue
-                blast_hit = ''.join(blast_hit)
+        if rounds == 'NULL':
+            new_file = 'results/blastout/sp_blast_extract/' + sp_out + '_' + CS_gene + '.fasta'
+        else:
+            new_file = 'results/blastout/round_' + rounds + '/sp_blast_extract/' + sp_out + '_' + CS_gene + '.fasta'
 
-                outF.write('>' + contig_hit + '\n' + blast_hit + '\n')
+        #with open('results/blastout/sp_blast_extract/' + sp_out + '_' + CS_gene + '.fasta', 'w') as outF:
+        with open(new_file, 'w') as outF:
+            with open(taxa) as f:
+                gene_contigs = defaultdict(list)
+                contig_seq_hit = defaultdict(list)
+
+                for line in f:
+                    lines = line.split('\t')
+                    #geneID = lines[0].split('|')[1]
+                    gene = lines[0]#.split('0')[0]
+                    if CS_gene in gene:
+
+                        sp_contig = lines[1]
+                        qstart = lines[5]
+                        qend = lines[6]
+                        qhit = int(qend) - int(qstart)
+                        qlen = lines[7]
+                        q_cov = qhit/int(qlen)
+                        q_cov = 100*q_cov
+
+                        s_start = lines[8]
+                        s_end = lines[9]
+                        s_len = lines[10]
+
+                        contig_seq_hit[sp_contig].append(int(s_start))
+                        contig_seq_hit[sp_contig].append(int(s_end))
+                        gene_contigs[gene].append(sp_contig)
+
+
+                for contig_hit, nuc_hit in contig_seq_hit.items():
+                    nuc_start = min(nuc_hit) - 100
+                    nuc_end = max(nuc_hit) + 100 #Regions within a contig that had multiple CS gene hits are taken out in a single large sequence for annotation
+
+                    #print(contig_hit, nuc_start, nuc_end)
+
+                    genome_contig = contig_seq[contig_hit]
+
+                    blast_hit = []
+                    for i in range(nuc_start, nuc_end):
+                        try:
+                            nuc = genome_contig[i]
+                            blast_hit.append(nuc)
+                        except:
+                            continue
+                    blast_hit = ''.join(blast_hit)
+
+                    outF.write('>' + contig_hit + '\n' + blast_hit + '\n')
 
                 
 if os.path.isdir('results/blastout/sp_blast_extract'):
